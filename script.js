f436d063fc06fb6785c49b3cb10c2ef386015b9b
25355180 <147062677+25355180@users.noreply.github.com>
2025-02-02 12:30:52 +0000

    fixing errors

--- script.js
+++ script.js
@@ -5,40 +5,40 @@ function myMap() {
   };
   }
   
-  function geocodeLatLng(geocoder, map, infowindow) {
-    const input = document.getElementById("latlng").value;
-    const latlngStr = input.split(",", 2);
-    const latlng = {
-      lat: parseFloat(latlngStr[0]),
-      lng: parseFloat(latlngStr[1]),
-    };
-  }
+function geocodeLatLng(geocoder, map, infowindow) {
+  const input = document.getElementById("latlng").value;
+  const latlngStr = input.split(",", 2);
+  const latlng = {
+    lat: parseFloat(latlngStr[0]),
+    lng: parseFloat(latlngStr[1]),
+  };
+}
 
-  const x = document.getElementById("demo");
+const x = document.getElementById("demo");
 
-    function getLocation() {
-    try {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } catch {
-        x.innerHTML = err;
-    }
-    }
+function getLocation() {
+try {
+    navigator.geolocation.getCurrentPosition(showPosition);
+} catch {
+    x.innerHTML = err;
+}
+}
 
-    function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude;
-    }
+function showPosition(position) {
+x.innerHTML = "Latitude: " + position.coords.latitude + 
+"<br>Longitude: " + position.coords.longitude;
+}
 
-    function initMap() {
-      // Create a map object centered at a specific location
-      var map = new google.maps.Map(document.getElementById('map'), {
-          center: { lat: 51.508742, lng: -0.120850 }, // Example coordinates
-          zoom: 10
-      });
-      
-      // Create a marker object
-      var marker = new google.maps.Marker({
-          position: { lat: 51.508742, lng: -0.120850 }, // Same coordinates as the map center
-          map: map,
-      });
-  }
+function initMap() {
+  // Create a map object centered at a specific location
+  var map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: 51.508742, lng: -0.120850 }, // Example coordinates
+      zoom: 10
+  });
+  
+  // Create a marker object
+  var marker = new google.maps.Marker({
+      position: { lat: 51.508742, lng: -0.120850 }, // Same coordinates as the map center
+      map: map,
+  });
+}
