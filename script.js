5ed90702e0dc7d23a57c41715d012746d5689a64
25355180 <147062677+25355180@users.noreply.github.com>
2025-02-06 10:44:29 +0000

    adding json

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ function convertToJson() {
   let jsonOutput = document.getElementById("jsonOutput");
   jsonOutput.innerHTML = "<pre>" + jsonData +"</pre>";
 }
+const xmlhttp = new XMLHttpRequest();
+xmlhttp.onload = function() {
+  const myObj = JSON.parse(this.responseText);
+  document.getElementById("Output").innerHTML = myObj.name;
+};
+xmlhttp.open("GET", "json_locations.txt");
+xmlhttp.send();
+
+
+
+const xmlhttp = new XMLHttpRequest();
+xmlhttp.onload = function() {
+  const myArr = JSON.parse(this.responseText);
+  document.getElementById("Output").innerHTML = myArr[0];
+}
+xmlhttp.open("GET", "json_locations.txt", true);
+xmlhttp.send();
