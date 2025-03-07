d3ff113db8f90f77e9553871d00637892c8cc60f
25355180 <147062677+25355180@users.noreply.github.com>
2025-03-07 14:22:24 +0000

    fixing

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function getData() {
     function displayNext() {
       if (index < locations.length) {
         const location = locations[index];
-        document.getElementById("Output").innerHTML = location.name + <br></br>; 
+        document.getElementById("Output").innerHTML = location.name + "<br>"; 
         showPositions(location); 
         index++; 
       }
